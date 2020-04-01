1973454308c586a226038a2d5a7f18d7635b30a6
jhon11390 <meljhon2@gmail.com>
2020-04-01 13:45:16 -0500

    Se crea constructor de repeticion de caracteres de input

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      words: ''
+    }
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" value={this.state.words} onChange={this.updateWords.bind(this)} />
+        <p className="repeater">{this.state.words}</p>
       </div>
     );
+  }
+  updateWords(event){
+    this.setState({
+      words: event.target.value
+    });
   }
 }
 
